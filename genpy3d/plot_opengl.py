26921ca1132256f8c4725d50d191caa6b51fe280
martin <mcbride.martin@gmail.com>
2026-03-26 15:28:56 +0000

    Make axes reversible

--- genpy3d/plot_opengl.py
+++ genpy3d/plot_opengl.py
@@ -30,6 +30,7 @@ class Axes:
     axis_line_width: float = 3
     div_colors: tuple = ((0.6, 0.6, 0.6),)*3
     div_line_width: float = 3
+    reverse_axis: tuple = (False, False, False)
 
     def of_start(self, start):
         self.start = start
@@ -48,7 +49,10 @@ class Axes:
         return self
 
     def transform_from_graph(self, point):
-        return [(point[i] - self.start[i]) * self.size[i] / self.extent[i] for i in range(3)]
+        return [((self.start[i] + self.extent[i] - point[i]) * self.size[i] / self.extent[i])
+                if self.reverse_axis[i] else
+                ((point[i] - self.start[i]) * self.size[i] / self.extent[i])
+                for i in range(3)]
 
     def set_axis_style(self, width=4, r=(1, 0, 0), g=(0, 1, 0), b=(0, 0, 1)):
         self.axis_line_width =width
@@ -57,6 +61,12 @@ class Axes:
     def set_div_style(self, width=3, r=(1, 0.5, 0.5), g=(0.5, 1, 0.5), b=(0.5, 0.5, 1)):
         self.div_line_width =width
         self.div_colors = (r, g, b)
+
+    def _get_device_start(self, axis):
+        return 0
+
+    def _get_device_end(self, axis):
+        return self.size[axis]
 
     def _get_divs(self, start, extent, div):
         close = abs(extent/10)
@@ -79,31 +89,31 @@ class Axes:
         markers = self._get_divs(self.start[0], self.extent[0], self.divs[0])
         for m in markers:
             pos = self.transform_from_graph((m, self.start[1]+self.extent[1], 0))
-            glVertex3f(pos[0], 0, 0)
-            glVertex3f(pos[0], pos[1], 0)
+            glVertex3f(pos[0], self._get_device_start(1), 0)
+            glVertex3f(pos[0], self._get_device_end(1), 0)
             pos = self.transform_from_graph((m, 0, self.start[2]+self.extent[2]))
-            glVertex3f(pos[0], 0, 0)
-            glVertex3f(pos[0], 0, pos[2])
+            glVertex3f(pos[0], 0, self._get_device_start(2))
+            glVertex3f(pos[0], 0, self._get_device_end(2))
 
         glColor3f(*self.div_colors[1])
         markers = self._get_divs(self.start[1], self.extent[1], self.divs[1])
         for m in markers:
             pos = self.transform_from_graph((self.start[0]+self.extent[0], m, 0))
-            glVertex3f(0, pos[1], 0)
-            glVertex3f(pos[0], pos[1], 0)
+            glVertex3f(self._get_device_start(0), pos[1], 0)
+            glVertex3f(self._get_device_end(0), pos[1], 0)
             pos = self.transform_from_graph((0, m, self.start[2]+self.extent[2]))
-            glVertex3f(0, pos[1], 0)
-            glVertex3f(0, pos[1], pos[2])
+            glVertex3f(0, pos[1], self._get_device_start(2))
+            glVertex3f(0, pos[1], self._get_device_end(2))
 
         glColor3f(*self.div_colors[2])
         markers = self._get_divs(self.start[2], self.extent[2], self.divs[2])
         for m in markers:
             pos = self.transform_from_graph((0, self.start[1]+self.extent[1], m))
-            glVertex3f(0, 0, pos[2])
-            glVertex3f(0, pos[1], pos[2])
+            glVertex3f(0, self._get_device_start(1), pos[2])
+            glVertex3f(0, self._get_device_end(1), pos[2])
             pos = self.transform_from_graph((self.start[0]+self.extent[0], 0, m))
-            glVertex3f(0, 0, pos[2])
-            glVertex3f(pos[0], 0, pos[2])
+            glVertex3f(self._get_device_start(0), 0, pos[2])
+            glVertex3f(self._get_device_end(0), 0, pos[2])
 
         glEnd()
 
@@ -119,22 +129,22 @@ class Axes:
         markers = self._get_divs(self.start[0], self.extent[0], self.divs[0])
         for m in markers:
             pos = self.transform_from_graph((m, self.start[1]+self.extent[1], 0))
-            glVertex3f(pos[0], pos[1], 0)
-            glVertex3f(pos[0], pos[1]+tick, 0)
+            glVertex3f(pos[0], self._get_device_end(1), 0)
+            glVertex3f(pos[0], self._get_device_end(1)+tick, 0)
 
         glColor3f(*self.axis_colors[1])
         markers = self._get_divs(self.start[1], self.extent[1], self.divs[1])
         for m in markers:
             pos = self.transform_from_graph((self.start[0]+self.extent[0], m, 0))
-            glVertex3f(pos[0], pos[1], 0)
-            glVertex3f(pos[0]+tick, pos[1], 0)
+            glVertex3f(self._get_device_end(0), pos[1], 0)
+            glVertex3f(self._get_device_end(0)+tick, pos[1], 0)
 
         glColor3f(*self.axis_colors[2])
         markers = self._get_divs(self.start[2], self.extent[2], self.divs[2])
         for m in markers:
             pos = self.transform_from_graph((0, self.start[1]+self.extent[1], m))
-            glVertex3f(0, pos[1], pos[2])
-            glVertex3f(0, pos[1]+tick, pos[2])
+            glVertex3f(0, self._get_device_end(1), pos[2])
+            glVertex3f(0, self._get_device_end(1)+tick, pos[2])
 
         glEnd()
 
@@ -145,14 +155,14 @@ class Axes:
         markers = self._get_divs(self.start[0], self.extent[0], self.divs[0])
         for m in markers:
             pos = self.transform_from_graph((m, self.start[1]+self.extent[1], 0))
-            glRasterPos3f(pos[0] + self.text_offset[0][0], pos[1] + self.text_offset[0][1], 0)
+            glRasterPos3f(pos[0] + self.text_offset[0][0], self._get_device_end(1)  + self.text_offset[0][1], 0)
             label = f"{m:.1f}"
             for c in label:
                 glutBitmapCharacter(GLUT_BITMAP_HELVETICA_12, ord(c))
 
         # Draw x axis label
         pos = self.transform_from_graph((self.start[0]+self.extent[0]/2, self.start[1]+self.extent[1], 0))
-        glRasterPos3f(pos[0] + self.axis_text_offset[0][0], pos[1] + self.axis_text_offset[0][1], 0)
+        glRasterPos3f(pos[0] + self.axis_text_offset[0][0], self._get_device_end(1) + self.axis_text_offset[0][1], 0)
         glutBitmapCharacter(GLUT_BITMAP_HELVETICA_12, ord("X"))
 
         # Draw Y tick labels
@@ -160,14 +170,14 @@ class Axes:
         markers = self._get_divs(self.start[1], self.extent[1], self.divs[1])
         for m in markers:
             pos = self.transform_from_graph((self.start[0]+self.extent[0], m, 0))
-            glRasterPos3f(pos[0] + self.text_offset[1][0], pos[1] + self.text_offset[1][1], 0)
+            glRasterPos3f(self._get_device_end(0) + self.text_offset[1][0], pos[1] + self.text_offset[1][1], 0)
             label = f"{m:.1f}"
             for c in label:
                 glutBitmapCharacter(GLUT_BITMAP_HELVETICA_12, ord(c))
 
         # Draw y axis label
         pos = self.transform_from_graph((self.start[0] + self.extent[0], self.start[1] + self.extent[1]/2, 0))
-        glRasterPos3f(pos[0] + self.axis_text_offset[1][0], pos[1] + self.axis_text_offset[1][1], 0)
+        glRasterPos3f(self._get_device_end(0) + self.axis_text_offset[1][0], pos[1] + self.axis_text_offset[1][1], 0)
         glutBitmapCharacter(GLUT_BITMAP_HELVETICA_12, ord("Y"))
 
         # Draw z tick labels
@@ -175,14 +185,14 @@ class Axes:
         markers = self._get_divs(self.start[2], self.extent[2], self.divs[2])
         for m in markers:
             pos = self.transform_from_graph((0, self.start[1]+self.extent[1], m))
-            glRasterPos3f(0, pos[1] + self.text_offset[2][1],  pos[2] + self.text_offset[2][2])
+            glRasterPos3f(0, self._get_device_end(1) + self.text_offset[2][1],  pos[2] + self.text_offset[2][2])
             label = f"{m:.1f}"
             for c in label:
                 glutBitmapCharacter(GLUT_BITMAP_HELVETICA_12, ord(c))
 
         # Draw z axis label
         pos = self.transform_from_graph((0, self.start[1]+self.extent[1], self.start[2]+self.extent[2]/2))
-        glRasterPos3f(0, pos[1] + self.axis_text_offset[2][1],  pos[2] + self.axis_text_offset[2][2])
+        glRasterPos3f(0, self._get_device_end(1) + self.axis_text_offset[2][1],  pos[2] + self.axis_text_offset[2][2])
         glutBitmapCharacter(GLUT_BITMAP_HELVETICA_12, ord("Z"))
 
 
