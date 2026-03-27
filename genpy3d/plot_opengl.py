2fcb1bf6ca8c1c7fc22809bdc3964f6ba91460b5
martin <mcbride.martin@gmail.com>
2026-03-27 13:34:53 +0000

    Basic plot, with range and scaling

--- genpy3d/plot_opengl.py
+++ genpy3d/plot_opengl.py
@@ -31,8 +31,6 @@ class Plot_z_of_xy:
     fore_colormap: Callable = get_bcgyr_color
     back_colormap: Callable = get_grey_color
 
-
-
     def of_function(self, func):
         self.plotfunc = func
         return self
@@ -44,12 +42,13 @@ class Plot_z_of_xy:
     def _clip(self):
         # Define 6 clipping planes for the axes cuboid
 
+        print(self.axes.size)
         # x >= 0  →  +x plane
         glClipPlane(GL_CLIP_PLANE0, [1.0, 0.0, 0.0, 0.0])
         glEnable(GL_CLIP_PLANE0)
 
         # x <= 1  →  -x + 1 >= 0
-        glClipPlane(GL_CLIP_PLANE1, [-1.0, 0.0, 0.0, 1.0])
+        glClipPlane(GL_CLIP_PLANE1, [-1.0, 0.0, 0.0, self.axes.size[0]])
         glEnable(GL_CLIP_PLANE1)
 
         # y >= 0
@@ -57,7 +56,7 @@ class Plot_z_of_xy:
         glEnable(GL_CLIP_PLANE2)
 
         # y <= 1
-        glClipPlane(GL_CLIP_PLANE3, [0.0, -1.0, 0.0, 1.0])
+        glClipPlane(GL_CLIP_PLANE3, [0.0, -1.0, 0.0, self.axes.size[1]])
         glEnable(GL_CLIP_PLANE3)
 
         # z >= 0
@@ -65,7 +64,7 @@ class Plot_z_of_xy:
         glEnable(GL_CLIP_PLANE4)
 
         # z <= 1
-        glClipPlane(GL_CLIP_PLANE5, [0.0, 0.0, -1.0, 1.0])
+        glClipPlane(GL_CLIP_PLANE5, [0.0, 0.0, -1.0, self.axes.size[2]])
         glEnable(GL_CLIP_PLANE5)
 
     def _unclip(self):
@@ -80,7 +79,8 @@ class Plot_z_of_xy:
         glColor3f(0.2, 0.7, 1.0)
 
         step = 0.02
-        range_min, range_max = 0, 1
+        x_range_min, x_range_max = self.axes.start[0], self.axes.start[0] + self.axes.extent[0]
+        y_range_min, y_range_max = self.axes.start[1], self.axes.start[1] + self.axes.extent[1]
 
         self._clip()
 
@@ -88,9 +88,9 @@ class Plot_z_of_xy:
 
         glCullFace(GL_BACK)
 
-        for x in np.linspace(range_min, range_min + range_max, self.precision):
+        for x in np.linspace(x_range_min, x_range_max, self.precision):
             glBegin(GL_TRIANGLE_STRIP)
-            for y in np.linspace(range_min, range_min + range_max, self.precision):
+            for y in np.linspace(y_range_min, y_range_max, self.precision):
                 z1 = self.plotfunc(x, y)
                 z2 = self.plotfunc(x + step, y)
 
@@ -102,9 +102,9 @@ class Plot_z_of_xy:
 
         glCullFace(GL_FRONT)
 
-        for x in np.linspace(range_min, range_min + range_max, self.precision):
+        for x in np.linspace(x_range_min, x_range_max, self.precision):
             glBegin(GL_TRIANGLE_STRIP)
-            for y in np.linspace(range_min, range_min + range_max, self.precision):
+            for y in np.linspace(y_range_min, y_range_max, self.precision):
                 z1 = self.plotfunc(x, y)
                 z2 = self.plotfunc(x + step, y)
 
